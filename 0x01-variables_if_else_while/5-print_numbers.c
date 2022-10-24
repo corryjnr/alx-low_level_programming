da7d92ce9ba52d63af521773ac3f0f466451effa
corryjnr <michaelcorry01@gmail.com>
2022-10-24 14:53:03 +0000

    flow control

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
