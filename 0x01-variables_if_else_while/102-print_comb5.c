4369a33b90dd1bb02baaed624860b16f82dfbf2f
corryjnr <michaelcorry01@gmail.com>
2022-10-24 19:58:04 +0000

    all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ for (i = 0; i <= 98; i++)
 		putchar(' ');
 		putchar((j / 10) + '0');
 		putchar((j % 10) + '0');
-		if (i == 98) && ( j == 99)
+		if (i == 98 && j == 99)
 			continue;
 
 		putchar(',');
