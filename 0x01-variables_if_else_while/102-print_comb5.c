04b9e6c767c2ae17733ee08010204d3466800af1
corryjnr <michaelcorry01@gmail.com>
2022-10-24 17:51:00 +0000

    all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+int n;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+		if (k >= i)
+		{
+			for (n = 0; n <= 9; n++)
+			{
+			if (n > j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(' ');
+				putchar(k + '0');
+				putchar(n + '0');
+
+				if ((n == 9) && (j == 8))
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
