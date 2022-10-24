f73a69e7d0af45b31a5ab79791ac7b09ba3358e5
corryjnr <michaelcorry01@gmail.com>
2022-10-24 09:47:56 +0000

    Printing to stderrr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- main block
+ * Return: 0
+ */
+int main(void)
+{
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 66, 1, stderr);
+return (1);
+}
